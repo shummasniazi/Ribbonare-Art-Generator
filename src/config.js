bc2f812079edbf729058700ee89dfc90dda2785f
fa15bs0117 <niyazishummas@gmail.com>
2022-04-17 21:30:22 +0500

    edited chnages

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ const baseUri = "ipfs://NewUriToReplace";
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "",
   creators: [
     {
       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
